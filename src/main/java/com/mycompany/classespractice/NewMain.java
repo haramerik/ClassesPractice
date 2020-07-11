78a12d245bfa214262b0b266a2ae942f6bf218a1
Haramerik <amerik.bains@gmail.com>
2020-07-11 14:06:42 -0700

    easy_classes

---
+++ src/main/java/com/mycompany/classespractice/NewMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.classespractice;
+
+/**
+ *
+ * @author haramerik
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Animal george = new Animal("George");
+        george.SayHi();
+        
+        Animal amy = new Animal("Amy");
+        amy.SayHi();
+        
+        Animal wolfie = new Dog("Wolfie");
+        wolfie.SayHi();
+              
+    }
+    
+}
